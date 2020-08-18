e112507be9bc5a2e1231e433653ccc275f318b6a
Alex Li <guohua_lee@hotmail.com>
2020-08-18 18:10:18 +1000

    fix endpoint and responsive (#71)
    Co-authored-by: GuoHua Li <ligu@GuoHuas-MacBook-Pro.local>

--- src/pages/components/Tryout/Tryout.js
+++ src/pages/components/Tryout/Tryout.js
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/theme-monokai";
 
-const playgroundUrl = "http://play.sysl.io/";
+const playgroundUrl = "https://sysl-playground.herokuapp.com/";
 const exampleList = [
   {
     name: "Sequence Diagram",
